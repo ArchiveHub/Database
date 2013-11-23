a52d42beab10adf1de47fff98334eddea1e94004
wangshilianghit <wangshilianghit@gmail.com>
2013-11-23 16:58:54 -0500

    commit

--- hw3/ShiliangWang_Hw3/Allpercentages.sql
+++ hw3/ShiliangWang_Hw3/Allpercentages.sql
@@ -1,11 +1,16 @@
 delimiter #
 
-CREATE PROCEDURE AllPercentages (IN password CHAR(20))
+CREATE PROCEDURE AllPercentages (IN password VARCHAR(15))
 BEGIN
+    DECLARE c INT;
+    SELECT COUNT(*) INTO C FROM Passwords WHERE CurPasswords = password;
 
-    SELECT Rawscores.SSN, 100 * Rawscores.HW1 / TotalPoints.HW1 AS HW1, 100 * Rawscores.HW2a / TotalPoints.HW2a AS HW2a, 100 * Rawscores.HW2b / TotalPoints.HW2b AS HW2b, 100 * Rawscores.Midterm / TotalPoints.Midterm AS Midterm, 100  * Rawscores.HW3 / TotalPoints.HW3 AS HW3, 100 * Rawscores.FExam / TotalPoints.FExam AS FExam FROM Rawscores, TotalPoints WHERE LName != 'TOTAL' and LName != 'WEIGHT';
+    IF c = 0
+        THEN SELECT 0;
+    ELSE   
+        SELECT Rawscores.SSN, FName, LName, Section, 100 * Rawscores.HW1 / TotalPoints.HW1 AS HW1, 100 * Rawscores.HW2a / TotalPoints.HW2a AS HW2a, 100 * Rawscores.HW2b / TotalPoints.HW2b AS HW2b, 100 * Rawscores.Midterm / TotalPoints.Midterm AS Midterm, 100  * Rawscores.HW3 / TotalPoints.HW3 AS HW3, 100 * Rawscores.FExam / TotalPoints.FExam AS FExam, 100 * ((Rawscores.HW1 * WtdPts.HW1) + (Rawscores.HW2a * WtdPts.HW2a) + (Rawscores.HW2b * WtdPts.HW2b) + (Rawscores.Midterm * WtdPts.Midterm) + (Rawscores.HW3 * WtdPts.HW3) + (Rawscores.FExam * WtdPts.FExam)) AS CumAvg FROM Rawscores JOIN TotalPoints JOIN WtdPts WHERE LName != 'TOTAL' and LName != 'WEIGHT' ORDER BY Section, CumAvg;
+    END IF;
 
-    SELECT FName, LName, 100 * ((Rawscores.HW1 * WtdPts.HW1) + (Rawscores.HW2a * WtdPts.HW2a) + (Rawscores.HW2b * WtdPts.HW2b) + (Rawscores.Midterm * WtdPts.Midterm) + (Rawscores.HW3 * WtdPts.HW3) + (Rawscores.FExam * WtdPts.FExam)) AS CumAvg FROM Rawscores JOIN WtdPts WHERE LName != 'TOTAL' and LName != 'WEIGHT';
 END#
 
 delimiter ;
--- hw3/ShiliangWang_Hw3/printpercentagescore.php
+++ hw3/ShiliangWang_Hw3/printpercentagescore.php
@@ -6,9 +6,9 @@
   // grab provided name from form. 
   $param_password = $_POST['password'];
 
-  // create query 
-  $query = "SELECT * FROM Passwords WHERE CurPaasswords = '" . $param_password . "')";
   
+  $query = "CALL Allpercentages('" . $param_password . "')"; 
+
   // Create connection
   $con=mysqli_connect("dbase.cs.jhu.edu","cs41513_wangshi","SPO901","cs41513_wangshi_db");
   echo $query . "<br>";
@@ -22,10 +22,75 @@
   {
     echo "Failed to connect to MySQL: " . mysql_connect_error();
   }
- 
-  
-  $query = "CALL 
+
+  if (
+?>
+
+<html>
+  <title>Question (b)</title>
+  <body>
+    <h3>Print a single student's percentage scores and wieghted average</h3>
+    <table border="1">
+      <tr>
+        <th>SSN</th>
+        <th>LName</th>
+        <th>FName</th>
+        <th>Section</th>
+        <th>HW1</th>
+        <th>HW2a</th>
+        <th>HW2b</th>
+        <th>Midterm</th>
+        <th>HW3</th>
+        <th>FExam</th>
+        <th>CumAvg</th>
+      </tr>
+<?php
+  while ($row = mysqli_fetch_array($result))
+  {
+?>
+      <tr>
+        <td>
+        <?php echo $row['SSN']; ?>
+        </td>
+        <td>
+        <?php echo $row['LName']; ?>
+        </td>
+        <td>
+        <?php echo $row['FName']; ?>
+        </td>
+        <td>
+        <?php echo $row['Section']; ?>
+        </td>
+        <td>
+        <?php echo round($row['HW1'], 2) . "%"; ?>
+        </td>
+        <td>
+        <?php echo round($row['HW2a'], 2) . "%"; ?>
+        </td>
+        <td>
+        <?php echo round($row['HW2b'], 2) . "%"; ?>
+        </td>
+        <td>
+        <?php echo round($row['Midterm'], 2) . "%"; ?>
+        </td>
+        <td>
+        <?php echo round($row['HW3'], 2) . "%"; ?>
+        </td>
+        <td>
+        <?php echo round($row['FExam'], 2) . "%"; ?>
+        </td>
+        <td>
+        <?php echo round($row['CumAvg'], 2) . "%"; ?>
+        </td>
+      </tr>
+<?php
+  }
 
   // close connection
   mysqli_close($con);
 ?>
+    </table>
+  </body>
+</html>
+
+
